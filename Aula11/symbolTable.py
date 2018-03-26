169faf60d9d4a54381f0b38f9cd56426dd38985e
Marcelo Andrade <marcelogdeandrade@gmail.com>
2018-03-26 19:15:30 -0300

    adds variables, code blocks and print to compiler

---
+++ Aula11/symbolTable.py
@@ -0,0 +1,11 @@
+class SymbolTable():
+    def __init__(self):
+        self.symbols = {}
+    def getSymbol(self, symbol):
+        if symbol in self.symbols.keys():
+            return self.symbols[symbol]
+        else:
+            #erro
+            pass
+    def setSymbol(self, symbol, value):
+        self.symbols[symbol] = value
---
+++ Aula11/token.py
@@ -0,0 +1,4 @@
+class Token():
+    def __init__(self, token_type, value):
+        self.type = token_type
+        self.value = value
